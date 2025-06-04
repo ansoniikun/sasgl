810ea7d2b1df94fa5de543a10ffbd1b0dde3b50a
HA Maleke <hm_utilities@tuta.com>
2025-06-04 17:04:32 +0200

    club description

--- app/clubdashboard/page.js
+++ app/clubdashboard/page.js
@@ -384,14 +384,16 @@ const ClubDashboard = () => {
           <h2 className="text-3xl font-semibold text-ash-gray mb-2">
             {clubData.name}
           </h2>
-          <p className="text-sm text-gray-500 max-w-[90%]">{`${clubData.description} Golf Doza Za is a welcoming and well-appointed golf club that offers an enjoyable experience for players of all skill levels. Known for its beautifully maintained course and relaxed atmosphere, the club is a perfect destination for both casual rounds and competitive play.`}</p>
+          <p className="text-sm text-gray-500 max-w-[90%]">
+            {clubData.description}
+          </p>
           <br></br>
-          <p className="hidden  text-gray-500 max-w-[90%]">
+          {/* <p className="hidden  text-gray-500 max-w-[90%]">
             With a commitment to quality and community, Golf Doza Za provides
             excellent facilities, including a full-length driving range, putting
             greens, a pro shop, and a comfortable clubhouse for post-game
             socialising.
-          </p>
+          </p> */}
         </div>
 
         <div className="mt-4 flex flex-col gap-3">
