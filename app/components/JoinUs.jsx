f37efcf23f5f656e7eb1b88a6c200c35e31ccb47
HA Maleke <hm_utilities@tuta.com>
2025-06-08 02:32:47 +0200

    changed join us section

--- app/components/JoinUs.jsx
+++ app/components/JoinUs.jsx
@@ -4,11 +4,18 @@ import Link from "next/link";
 
 const JoinUs = () => {
   return (
-    <section className="relative h-[40vh] w-full bg-black">
+    <section
+      className="relative h-[40vh] w-full bg-cover bg-center"
+      style={{ backgroundImage: "url('/join.png')" }}
+    >
+      {/* Overlay for better text visibility */}
+      <div className="absolute inset-0 bg-dark-green h-[80%] my-auto" />
       {/* Content */}
-      <div className="h-full flex flex-col justify-center items-center text-center px-4 text-white">
+      <div className="relative z-10 h-full flex flex-col justify-center items-center text-center px-4 text-white">
         <h2 className="text-3xl md:text-4xl ">
-          <span className="text-dark-gold">Join</span> Club Now
+          <span className="text-dark-gold">Golfers</span> don’t just play in
+          tournaments,{" "}
+          <span className="text-dark-gold">they seek experiences</span>
         </h2>
         <div className="w-5 h-1 bg-dark-gold mt-3 mb-4" />
         <p className="text-lg md:text-xl max-w-2xl">
